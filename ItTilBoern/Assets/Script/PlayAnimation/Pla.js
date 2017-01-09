b7c2896a7f0034afa2a6d015bcaf1a1c81fb7408
emilnt1 <emilbfh@hotmail.com>
2017-01-09 14:21:27 +0100

    Animation of tree #DONE

---
+++ ItTilBoern/Assets/Script/PlayAnimation/Pla.js
@@ -0,0 +1,36 @@
+﻿var ani : Animator;
+var sound : AudioClip;
+private var enter : boolean;
+ 
+function Start () {
+    ani.enabled = false;
+}
+
+function Update(){
+    if(Input.GetMouseButton(0) && enter){
+        OnMouseDown();
+    }
+}
+function OnMouseDown () {
+    AudioSource.PlayClipAtPoint(sound, transform.position);
+    ani.enabled = true;
+    {
+        yield WaitForSeconds (5);
+        ani.enabled = false;
+
+    }   
+}
+
+//Activate the Main function when player is near the door
+function OnTriggerEnter (other : Collider){
+    if (other.gameObject.tag == "Player") {
+        enter = true;
+    }
+}
+
+    //Deactivate the Main function when player is go away from door
+    function OnTriggerExit (other : Collider){
+        if (other.gameObject.tag == "Player") {
+            enter = false;
+        }
+    }

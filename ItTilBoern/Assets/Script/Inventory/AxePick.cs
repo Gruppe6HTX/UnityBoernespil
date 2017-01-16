825f80baaec92a6a9ada203e54dc1b7c84edc952
emilnt1 <emilbfh@hotmail.com>
2017-01-16 14:58:50 +0100

    KlippeAfsats

---
+++ ItTilBoern/Assets/Script/Inventory/AxePick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxePick : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Part")
+        {
+
+            collision.transform.parent = transform;
+
+        }
+    }
+}
+

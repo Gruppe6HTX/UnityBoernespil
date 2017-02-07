1065f5b801c32816044f816387d6d5d996bffe5d
emilnt1 <emilbfh@hotmail.com>
2017-02-07 10:42:39 +0100

    controller

---
+++ ItTilBoern/Assets/Script/Inventory/AxePick.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+var onhand : Transform;
+
+function Start () {
+	
+}
+
+function Update () {
+	
+}
+function OnMouseDown(){
+    GetComponent.<Rigidbody>().useGravity=false;
+    this.transform.position = onhand.position;
+    
+}
